99540fc617870c9388083cf938d2edbf45bd29bc
ranqiu <ranqiu.he@tuya.com>
2022-08-05 01:17:50 +0800

    fix: 增加猫的动画

--- src/cat/index.js
+++ src/cat/index.js
@@ -5,13 +5,22 @@ function Cat() {
   return (
     <div className="drawing-holder">
       <div className="cat-face">
+        <div className="cat-left-ear"></div>
+        <div className="cat-right-ear"></div>
         <div className="cat-hair-1"></div>
         <div className="cat-hair-2"></div>
         <div className="cat-hair-3"></div>
         <div className="cat-eyes"></div>
         <div className="cat-nose"></div>
         <div className="cat-mouth"></div>
+        <div className="cat-mouth-up"></div>
         <div className="cat-beard-1"></div>
+        <div className="cat-beard-2"></div>
+        <div className="cat-beard-3"></div>
+        <div className="cat-beard-4"></div>
+        <div className="cat-beard-5"></div>
+        <div className="cat-beard-6"></div>
+        <div className="cat-sound">喵～</div>
       </div>
     </div>
   );
